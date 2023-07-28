c88330f5e86e74bc669894a5d4247ccc08c97d64
kumapower17 <kumapowerd@gmail.com>
2023-07-28 23:20:28 +0800

    fix typo

--- chapter01/hello_server.c
+++ chapter01/hello_server.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
     serv_addr.sin_port=htons(atoi(argv[1]));
 
-    if(bind(serv_sock, (struct sockeraddr*)&serv_addr, sizeof(serv_addr))==-1)
+    if(bind(serv_sock, (const struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
         error_handling("bind() error");
 
     if(listen(serv_sock, 5)==-1)
